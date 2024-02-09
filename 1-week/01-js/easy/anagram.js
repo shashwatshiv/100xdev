5f571822a72adb55ee672e549ddc3203b95ab24b
Shashwat Shivhare <shashwat02infinity@gmail.com>
2024-02-09 23:27:23 +0530

    weeek1 assignment

--- 1-week/01-js/easy/anagram.js
+++ 1-week/01-js/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
-
+  console.log(str1.sort());
+  JSON.stringify(str1, "");
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  let arr1 = str1.split("");
+  let arr2 = str2.split("");
+  arr1.sort();
+  arr2.sort();
+  console.log(JSON.stringify(arr1));
+  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+    return true;
+  } else return false;
 }
-
+let to = isAnagram("sdfasdjf adsfs", "sdfsd");
 module.exports = isAnagram;
--- 1-week/01-js/easy/expenditure-analysis.js
+++ 1-week/01-js/easy/expenditure-analysis.js
@@ -1,20 +1,22 @@
-/*
-  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
-  and return a list of objects where each object is unique category-wise and has total price spent as its value.
-  transactions is an array where each
-  Transaction - an object like 
-        {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
-  Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
+/* Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
+and return a list of objects where each object is unique category-wise and has total price spent as its value.
+transactions is an array where each
+Transaction - an object like{
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  }
+Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+  let Output = [];
+  for (transaction of transactions) {
+    for (let i = 0; i < Output.length; i++) {}
 
+    return Output;
+  }
+}
 module.exports = calculateTotalSpentByCategory;
